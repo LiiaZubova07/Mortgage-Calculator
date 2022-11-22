e86cd2accd754e8b35a2321fb700d1c11970954b
Liia Zubova <vardlitmo@gmail.com>
2022-11-22 20:09:33 +0300

    add: generate event for clients

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -21,7 +21,21 @@ function init(getData) {
   radioBtns.forEach(function (radioBtn) {
     radioBtn.addEventListener('change', function () {
       console.log(this);
-      console.log(parseFloat(this.value));
+      //преобразование в число
+      console.log(parseFloat(parseFloat(this.value)));
+      console.log(this.id);
+
+      //генерация пользовательского события
+      this.dispatchEvent(
+        new CustomEvent('updateForm', {
+          bubbles: true,
+          detail: {
+            selectedProgram: parseFloat(this.value),
+            onUpdate: 'radioProgram',
+				id: this.id,
+          },
+        })
+      );
     });
   });
 }
