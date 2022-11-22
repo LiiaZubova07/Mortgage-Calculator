3f558dcc8dcd632eafaf7a2967690ab32e80f161
Liia Zubova <vardlitmo@gmail.com>
2022-11-22 17:43:30 +0300

    add: init programs

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,11 @@
 import * as Model from './model.js';
+import programs from './view/radioPrograms.js';
 
 //весь код в контроллере будет запускаться, кода всё загружено
 window.onload = function () {
 	const getData = Model.getData;
 
 //отображение программ на странице(ХХ)
-
+//init programs
+programs(getData);
 };
---
+++ js/view/radioPrograms.js
@@ -0,0 +1,8 @@
+//код, который будет инициализировать ставки(ХХ)
+
+function init(getData) {
+	console.log("Init programs");
+	console.log(getData());
+}
+
+export default init;
