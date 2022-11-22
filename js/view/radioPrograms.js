706c4208f09196595364ceed0ce0ca84e9f07f03
Liia Zubova <vardlitmo@gmail.com>
2022-11-22 17:50:50 +0300

    add: Set program rates in radio buttons

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -1,8 +1,14 @@
 //код, который будет инициализировать ставки(ХХ)
 
 function init(getData) {
-	console.log("Init programs");
-	console.log(getData());
+  console.log('Init programs');
+  console.log(getData().programs);
+
+  const { base, it, gov, zero } = getData().programs;
+
+  //Устанавливаю эти значения в радиокнопку
+  //Set program rates in radio buttons
+  document.querySelector('#base-value').value = base;
 }
 
 export default init;
