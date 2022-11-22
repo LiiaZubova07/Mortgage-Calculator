a3154069470fe4a56733f191ce31d1b5499a5e43
Liia Zubova <vardlitmo@gmail.com>
2022-11-22 17:36:50 +0300

    add: import & onload

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+import * as Model from './model.js';
+
+//весь код в контроллере будет запускаться, кода всё загружено
+window.onload = function () {
+	const getData = Model.getData;
+
+//отображение программ на странице(ХХ)
+
+};
