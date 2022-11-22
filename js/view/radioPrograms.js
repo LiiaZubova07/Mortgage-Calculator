7d65bca969979563ab87bb20161e7a44c129b9de
Liia Zubova <vardlitmo@gmail.com>
2022-11-22 17:57:30 +0300

    add: Set program rates in radio buttons for clients

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -13,10 +13,11 @@ function init(getData) {
   document.querySelector('#gov-value').value = gov;
   document.querySelector('#zero-value').value = zero;
 
-  document.querySelector('#base-text').innerText = base;
-  document.querySelector('#it-text').innerText = it;
-  document.querySelector('#gov-text').innerText = gov;
-  document.querySelector('#zero-text').innerText = zero;
+  //Show program rates on page
+  document.querySelector('#base-text').innerText = base * 100 + '%';
+  document.querySelector('#it-text').innerText = it * 100 + '%';
+  document.querySelector('#gov-text').innerText = gov * 100 + '%';
+  document.querySelector('#zero-text').innerText = zero * 100 + '%';
 }
 
 export default init;
