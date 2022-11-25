322c4d910a37e25c55ca08072c367a1c50a3eae1
Liia Zubova <vardlitmo@gmail.com>
2022-11-25 18:33:47 +0300

    replace: getData() to data

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,4 +1,6 @@
 function init(getData) {
+  const data = getData();
+
   //   console.log('FIRED');
   const input = document.querySelector('#input-cost');
 
@@ -10,7 +12,7 @@ function init(getData) {
 
   const cleaveInput = new Cleave(input, settings);
   //установка стартового значения
-  cleaveInput.setRawValue(getData().cost);
+  cleaveInput.setRawValue(data.cost);
 
   //прослушка на ввод стоимости
   input.addEventListener('input', function () {
@@ -18,11 +20,11 @@ function init(getData) {
     console.log(value);
 
     //проверка на минимальную и максимальную цену
-    if (value < getData().minPrice || value > getData().maxPrice) {
+    if (value < data.minPrice || value > data.maxPrice) {
       input.closest('.param__details').classList.add('param__details--error');
     }
 
-    if (value >= getData().minPrice && value <= getData().maxPrice) {
+    if (value >= data.minPrice && value <= data.maxPrice) {
       input
         .closest('.param__details')
         .classList.remove('param__details--error');
@@ -33,17 +35,17 @@ function init(getData) {
   input.addEventListener('change', function () {
     const value = +cleaveInput.getRawValue();
 
-    if (value > getData().maxPrice) {
+    if (value > data.maxPrice) {
       input
         .closest('.param__details')
         .classList.remove('param__details--error');
-      cleaveInput.setRawValue(getData().maxPrice);
+      cleaveInput.setRawValue(data.maxPrice);
     }
-    if (value < getData().minPrice) {
+    if (value < data.minPrice) {
       input
         .closest('.param__details')
         .classList.remove('param__details--error');
-      cleaveInput.setRawValue(getData().minPrice);
+      cleaveInput.setRawValue(data.minPrice);
     }
   });
 }
