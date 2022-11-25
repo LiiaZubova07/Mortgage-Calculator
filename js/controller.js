22fedd69ada28839fc04343a180e8fd73bad31c6
Liia Zubova <vardlitmo@gmail.com>
2022-11-25 17:01:24 +0300

    add & init: costInput

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,8 @@
 import * as Model from './model.js';
 import updateResultsView from './view/updateResultsView.js';
+
 import programs from './view/radioPrograms.js';
+import costInput from './view/costinput.js';
 
 //весь код в контроллере будет запускаться, кода всё загружено
 window.onload = function () {
@@ -9,15 +11,17 @@ window.onload = function () {
   //init programs
   programs(getData);
 
+  //инициализирую запуская costInput
+  costInput();
   //отслеживать, что генерация идёт (прослушка пользовательского события)
   document.addEventListener('updateForm', (e) => {
     //в модели будет ф-я, которая будет обновлять данные
     Model.setData(e.detail);
 
-	 const data = Model.getData();
-	 const results = Model.getResults();
+    const data = Model.getData();
+    const results = Model.getResults();
 
-	 //Обновляю блок с результатами
-	 updateResultsView(results);
+    //Обновляю блок с результатами
+    updateResultsView(results);
   });
 };
