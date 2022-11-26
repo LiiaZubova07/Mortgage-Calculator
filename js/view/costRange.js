09953e724b9716092019af3a2cd6fc810ada4f7f
Liia Zubova <vardlitmo@gmail.com>
2022-11-26 17:45:53 +0300

    add: rangeSlider update the model

--- js/view/costRange.js
+++ js/view/costRange.js
@@ -1,3 +1,5 @@
+import updateModel from './../utils/updateModel.js';
+
 function init(getData) {
   const slider = document.querySelector('#slider-cost');
   const data = getData();
@@ -18,6 +20,20 @@ function init(getData) {
       thousand: ' ',
       suffix: '',
     }),
+  });
+
+  //следит за событиями slide
+  slider.noUiSlider.on('slide', function () {
+    //получить значение слайдера
+    let sliderValue = slider.noUiSlider.get();
+    //избавить от точки и пробелов в консоли
+    sliderValue = sliderValue.split('.')[0];
+    sliderValue = parseInt(String(sliderValue).replace(/ /g, ''));
+
+    updateModel(slider, {
+      cost: sliderValue,
+      onUpdate: 'costSlider',
+    });
   });
 }
 
