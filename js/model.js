9cab4fc926472352f48b506686391e82ea87c469
Liia Zubova <vardlitmo@gmail.com>
2022-11-26 20:41:02 +0300

    add: calculation of minimum persents

--- js/model.js
+++ js/model.js
@@ -3,6 +3,8 @@ let data = {
   cost: 10000000,
   minPrice: 375000,
   maxPrice: 100000000,
+  minPaymentPercents: 0.15,
+  maxPaymentPercents: 0.9,
   programs: {
     base: 0.1,
     it: 0.047,
@@ -28,6 +30,24 @@ function getResults() {
 
 function setData(newData) {
   console.log('New Data', newData);
+
+  if (newData.onUpdate === 'radioProgram') {
+    if (newData.id === 'zero-value') {
+      data.minPaymentPercents = 0;
+    } else {
+      data.minPaymentPercents = 0.15;
+    }
+
+    if (newData.onUpdate === 'inputCost') {
+      // Обновление цены
+      // Если стоимость меньше мин цены
+      if (newData.cost < data.minPrice) newData.cost = data.minPrice;
+
+      // Если стоимость больше макс цены
+      if (newData.cost > data.maxPrice) newData.cost = data.maxPrice;
+    }
+  }
+
   data = {
     ...data,
     ...newData,
