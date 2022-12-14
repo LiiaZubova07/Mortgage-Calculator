8f7f22ae39c099d2ffc6911e3b2cfbdd2f0c6812
Liia Zubova <vardlitmo@gmail.com>
2022-12-14 18:35:28 +0300

    add: file timeInput.js

---
+++ js/view/timeInput.js
@@ -0,0 +1,66 @@
+import updateModel from './../utils/updateModel.js';
+
+function init(getData) {
+  const data = getData();
+  //   console.log('FIRED');
+  const input = document.querySelector('#input-term');
+
+  const settings = {
+    numeral: true,
+    numeralThousandsGroupStyle: 'thousand',
+    delimiter: ' ',
+  };
+
+  const cleaveInput = new Cleave(input, settings);
+  //установка стартового значения
+  cleaveInput.setRawValue(data.cost);
+
+  //прослушка на ввод стоимости
+  input.addEventListener('input', function () {
+    const value = +cleaveInput.getRawValue();
+    console.log(value);
+
+    //проверка на минимальную и максимальную цену
+    if (value < data.minPrice || value > data.maxPrice) {
+      input.closest('.param__details').classList.add('param__details--error');
+    }
+
+    if (value >= data.minPrice && value <= data.maxPrice) {
+      input
+        .closest('.param__details')
+        .classList.remove('param__details--error');
+    }
+
+    //обновить модель
+    updateModel(input, {
+      cost: value,
+      onUpdate: 'inputCost',
+    });
+  });
+
+  //когда ввели маленькое число будет сбрасываться на минимально допустимое
+  input.addEventListener('change', function () {
+    const value = +cleaveInput.getRawValue();
+
+    if (value > data.maxPrice) {
+      input
+        .closest('.param__details')
+        .classList.remove('param__details--error');
+      cleaveInput.setRawValue(data.maxPrice);
+    }
+    if (value < data.minPrice) {
+      input
+        .closest('.param__details')
+        .classList.remove('param__details--error');
+      cleaveInput.setRawValue(data.minPrice);
+    }
+    //обновить модель
+    updateModel(input, {
+      cost: value,
+      onUpdate: 'inputCost',
+    });
+  });
+  return cleaveInput;
+}
+
+export default init;
